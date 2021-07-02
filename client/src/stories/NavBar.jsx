b648650fba83b3f5a37687b0e1d61c7c5231529f
JettScythe <jettscythe@hotmail.com>
2021-07-02 11:19:34 -0300

    add react-icons, add NavBar story
    Co-authored-by: Zain Dewsi <zaindewsi@users.noreply.github.com>
    Co-authored-by: Vitaly Zaff <vitalyzaff@users.noreply.github.com>

--- client/src/stories/NavBar.jsx
+++ client/src/stories/NavBar.jsx
@@ -1,9 +1,10 @@
-import React from "react";
 import Logo from "./assets/guap.png";
 import "./NavBar.scss";
+import { BiCog } from "react-icons/bi";
 
 export const NavBar = () => (
   <header className="navbar">
     <img src={Logo} className="logo" alt="guap" />
+    <BiCog className="settings-icon" />
   </header>
 );
--- client/src/stories/NavBar.stories.jsx
+++ client/src/stories/NavBar.stories.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import { NavBar } from "./NavBar";
 
 export default {
