a70d8bf7ac8b493996a3bb6c6815926979db426f
vitalyzaff <vitalyzavgorodnev@gmail.com>
2021-07-04 01:16:59 -0400

    create coin table component

---
+++ client/src/components/Coins/CoinTable.jsx
@@ -0,0 +1,54 @@
+import  CoinGecko from 'coingecko-api';
+import { useEffect, useState } from 'react';
+import "./CoinTable.scss";
+
+export default function CoinItemList () {
+  const CoinGeckoClient = new CoinGecko();
+  const [coins, setCoins] = useState([]);
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+  const numColor = (num) => {
+    if (num[0] === '-') {
+      return (
+        <div style={{ color: 'red' }}>
+          {num}
+        </div>
+      );
+    }
+    return (
+      <div style={{ color: 'green' }}>
+        {num}
+      </div>
+    );
+  };
+  useEffect(() => {
+    const getCoinData = async () => await CoinGeckoClient.coins.markets();
+    getCoinData().then(res => setCoins(res.data));
+  }, []);
+  return (
+    <table className="coins-table">
+      <thead>
+        <tr>
+          <th>Rank</th>
+          <th>Name</th>
+          <th>Price</th>
+          <th>Market Cap</th>
+          <th>24hr change</th>
+        </tr>
+      </thead>
+      {coins.map((coin) => (
+        <tbody className="coin-rows">
+          <tr>
+            <td>{coin.market_cap_rank}</td>
+            <td>{coin.name}</td>
+            <td>{formatter.format(coin.current_price)}</td>
+            <td>{formatter.format(coin.market_cap)}</td>
+            <td>{numColor(Number(coin.price_change_percentage_24h / 100).toLocaleString(undefined, { style: 'percent', minimumFractionDigits: 2 })) }</td>
+          </tr>
+        </tbody>
+      ))}
+    </table>
+  )
+};
