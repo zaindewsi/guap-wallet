cb57961507c834d7cb98bb23babe593af1991c3c
JettScythe <jettscythe@hotmail.com>
2021-07-04 22:56:40 -0300

    add Coin Table page

--- client/src/components/Body.jsx
+++ client/src/components/Body.jsx
@@ -1,7 +1,7 @@
 import "./Body.scss";
 import Wallet from "./Wallet";
 import { Switch, Route } from "react-router-dom";
-import CoinTable from "./Coins/CoinTable"
+import CoinTable from "./Coins/CoinTable";
 
 const Body = () => {
   return (
@@ -11,7 +11,6 @@ const Body = () => {
           <Wallet />
         </Route>
         <Route exact path="/coins">
-          <h1> Coins </h1>
           <CoinTable />
         </Route>
         <Route exact path="/about">
--- client/src/components/Coins/CoinTable.jsx
+++ client/src/components/Coins/CoinTable.jsx
@@ -1,38 +1,36 @@
-import  CoinGecko from 'coingecko-api';
-import { useEffect, useState } from 'react';
+import CoinGecko from "coingecko-api";
+import { useEffect, useState } from "react";
 import "./CoinTable.scss";
-import { MdStars } from "react-icons/md"
+import { FaStar } from "react-icons/fa";
 
-export default function CoinItemList () {
+export default function CoinItemList() {
   const CoinGeckoClient = new CoinGecko();
   const [coins, setCoins] = useState([]);
-  const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "CAD",
   });
   const numColor = (num) => {
-    if (num[0] === '-') {
-      return (
-        <div style={{ color: 'red' }}>
-          {num}
-        </div>
-      );
+    if (num[0] === "-") {
+      return <div style={{ color: "red" }}>{num}</div>;
     }
-    return (
-      <div style={{ color: 'green' }}>
-        {num}
-      </div>
-    );
+    return <div style={{ color: "green" }}>{num}</div>;
   };
   useEffect(() => {
-    const getCoinData = async () => await CoinGeckoClient.coins.markets();
-    getCoinData().then(res => setCoins(res.data));
+    const getCoinData = async () =>
+      await CoinGeckoClient.coins.markets({
+        order: "market_cap_desc",
+        per_page: 100,
+        vs_currency: "cad",
+      });
+    getCoinData().then((res) => setCoins(res.data));
   }, []);
   return (
     <table className="coins-table">
       <thead>
         <tr>
           <th>Rank</th>
+          <th></th>
           <th>Name</th>
           <th>Price</th>
           <th>Market Cap</th>
@@ -42,16 +40,33 @@ export default function CoinItemList () {
       </thead>
       {coins.map((coin) => (
         <tbody>
-          <tr> 
+          <tr>
             <td>{coin.market_cap_rank}</td>
-            <td id="logo-name"><img className="coin-logo" src={coin.image} alt={coin.name}/> {coin.name}</td>
+            <td id="logo-name">
+              <img className="coin-logo" src={coin.image} alt={coin.name} />{" "}
+            </td>
+            <td>
+              <p>{coin.name}</p>
+            </td>
             <td>{formatter.format(coin.current_price)}</td>
             <td>{formatter.format(coin.market_cap)}</td>
-            <td>{numColor(Number(coin.price_change_percentage_24h / 100).toLocaleString(undefined, { style: 'percent', minimumFractionDigits: 2 })) }</td>
-            <td><MdStars className="star"/></td>
+            <td>
+              {numColor(
+                Number(coin.price_change_percentage_24h / 100).toLocaleString(
+                  undefined,
+                  {
+                    style: "percent",
+                    minimumFractionDigits: 2,
+                  }
+                )
+              )}
+            </td>
+            <td>
+              <FaStar className="star" />
+            </td>
           </tr>
         </tbody>
       ))}
     </table>
-  )
-};
+  );
+}
