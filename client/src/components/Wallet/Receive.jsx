4439c5642c32f4d9497b4debb0ea388b1e941a18
JettScythe <jettscythe@hotmail.com>
2021-07-06 14:12:52 -0300

    add copy to clipboard functionality
    Co-authored-by: vitalyzaff <vitalyzavgorodnev@gmail.com>
    Co-authored-by: Zain Dewsi <zain.dewsi@gmail.com>

--- client/src/components/Wallet/Receive.jsx
+++ client/src/components/Wallet/Receive.jsx
@@ -1,21 +1,26 @@
-import { FaWallet } from "react-icons/fa";
+import { FaWallet, FaCopy } from "react-icons/fa";
 import QRCode from "qrcode.react";
 
 const Receive = (props) => {
+  const copy = () => {
+    if (props.toggle) {
+      navigator.clipboard.writeText(props.slpAddress);
+    } else {
+      navigator.clipboard.writeText(props.cashAddress);
+    }
+  };
+
   return (
     <div className="receive">
       <h1>
         <FaWallet />
         Receive
       </h1>
-      <QRCode value={props.toggle ? props.slpAddress : props.cashAddress} onClick={() =>  {
-        if (props.toggle) {
-        navigator.clipboard.writeText(props.slpAddress)
-      } else {
-        navigator.clipboard.writeText(props.cashAddress)
-      }
-      }} 
-        />
+      <QRCode value={props.toggle ? props.slpAddress : props.cashAddress} />
+      <button onClick={copy}>
+        <FaCopy />
+      </button>
+
       <p>{props.toggle ? props.slpAddress : props.cashAddress}</p>
     </div>
   );
